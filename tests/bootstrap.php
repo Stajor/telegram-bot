27b47e9cdbba48a1d28982ed7fd7839f7154e43e
Alex Belenky <alex.belenky@appendad.com>
2018-06-14 15:17:40 +0300

    Add phpunit configuration

---
+++ tests/bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Set error reporting to the max level.
+ */
+error_reporting(-1);
+
+/*
+ * Set UTC timezone.
+ */
+date_default_timezone_set('UTC');
+
+$autoloader = __DIR__ . '/../vendor/autoload.php';
+
+/*
+ * Check that composer installation was done.
+ */
+if (!file_exists($autoloader)) {
+    throw new Exception(
+        'Please run "composer install" in root directory to setup unit test dependencies before running the tests'
+    );
+}
+
+// Include the Composer autoloader.
+require_once $autoloader;
+
+/*
+ * Unset global variables that are no longer needed.
+ */
+unset($autoloader);
