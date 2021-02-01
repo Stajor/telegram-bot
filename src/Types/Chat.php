40c2bcf48ce2b7db2ffde47337c6904cf4cf385b
Alex B <alex.belenky@me.com>
2021-02-01 21:33:58 +0200

    FIX "Typed property must not be accessed before initialization"

--- src/Types/Chat.php
+++ src/Types/Chat.php
@@ -11,16 +11,16 @@ class Chat extends Type {
 
     public int $id;
     public string $type;
-    public ?string $title;
-    public ?string $username;
-    public ?string $first_name;
-    public ?string $last_name;
-    public ?ChatPhoto $photo;
-    public ?string $description;
-    public ?string $invite_link;
-    public ?Message $pinned_message;
-    public ?ChatPermissions $permissions;
-    public ?int $slow_mode_delay;
-    public ?string $sticker_set_name;
-    public ?bool $can_set_sticker_set;
+    public ?string $title = null;
+    public ?string $username = null;
+    public ?string $first_name = null;
+    public ?string $last_name = null;
+    public ?ChatPhoto $photo = null;
+    public ?string $description = null;
+    public ?string $invite_link = null;
+    public ?Message $pinned_message = null;
+    public ?ChatPermissions $permissions = null;
+    public ?int $slow_mode_delay = null;
+    public ?string $sticker_set_name = null;
+    public ?bool $can_set_sticker_set = null;
 }
